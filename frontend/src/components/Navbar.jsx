338cfc9826e694a50497de39692d6f70d7307b72
Angel Aquino <aa23-0272@unphu.edu.do>
2025-03-18 16:08:15 -0400

    add new features
    new features

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,28 +1,35 @@
-import React from 'react'
+import React from 'react';
 import { IconShoppingCart } from '@tabler/icons-react';
+import { IconHeart } from '@tabler/icons-react';
 
 const Navbar = () => {
   return (
-    <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
-      <img src="/logo.PNG" alt="logo"  width={70}  height={55} className=' mx-4'/>
-      <header class="fixed top-0 z-10 flex items-center justify-center w-full mx-auto mt-3" >
-        <nav class="flex px-3 text-medium sm:font-sm font-medium rounded-full text-gray-900 justify-center items-center " >
-            <a class="relative block px-2 py-3 transition hover:text-gray-500 " aria-label="experiencia" href="/" > Home </a>
-            <a class="relative block px-2 py-3 transition hover:text-gray-500 " aria-label="proyectos" href="/About" > About </a>
-            <a class="relative block px-2 py-3 transition hover:text-gray-500 " aria-label="sobre-mi" href="/Contact" > Contact US</a>
-            <a class="relative block px-2 py-3 transition hover:text-gray-500 " aria-label="contacto" href="/ShopAll" > Shop All </a>
+    <header className='fixed top-0 z-10 w-full'>
+      <div className='max-w-7xl mx-auto flex justify-between items-center p-2'>
+        <img src="/logo.PNG" alt="logo" width={70} height={55} className='mx-4' />
+        
+        <nav className='flex space-x-4 text-gray-900'>
+          <a className='relative block px-2 py-3 transition hover:text-gray-500' aria-label="Home" href="/">Home</a>
+          <a className='relative block px-2 py-3 transition hover:text-gray-500' aria-label="About" href="/About">About</a>
+          <a className='relative block px-2 py-3 transition hover:text-gray-500' aria-label="Contact" href="/Contact">Contact Us</a>
+          <a className='relative block px-2 py-3 transition hover:text-gray-500' aria-label="Shop all" href="/ShopAll">Shop All</a>
         </nav>
+
+        <div className='flex items-center space-x-4'>
+          <a href="/Wishlist" className='flex justify-center items-center transition hover:text-gray-500' aria-label="Wishlist">
+            <IconHeart stroke={2} width={30} height={30} />
+          </a>
+          <a href="/Cart" className='flex justify-center items-center transition hover:text-gray-500' aria-label="Cart">
+            <IconShoppingCart stroke={2} width={30} height={30} />
+          </a>
+        </div>
+      </div>
     </header>
-    <div w-full flex justify-between items-center>
-    <a href="/Cart" class="flex flex-1 relative justify-end items-center transition hover:text-gray-500 z-50" aria-label="cart">
-          <IconShoppingCart stroke={2} width={30} height={30}/>
-    </a>
-    </div>
-    </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
+
 
 
 
