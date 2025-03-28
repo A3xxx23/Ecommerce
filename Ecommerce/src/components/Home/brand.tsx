e54621f23759aad22606b1458dbef5a957c28768
Angel Aquino <aa23-0272@unphu.edu.do>
2025-03-28 14:08:59 -0400

    add new style

--- Ecommerce/src/components/Home/brand.tsx
+++ Ecommerce/src/components/Home/brand.tsx
@@ -1,66 +1,78 @@
 const brands = [
-    
     {
         image: '/brands/chrome.png',
         alt: 'Chrome Hearts',
+        url: 'https://www.chromehearts.com'
     },
     {
         image: '/brands/denimTears.png',
         alt: 'Denim Tears',
+        url: 'https://denimtears.com/do/'
     },
     {
         image: '/brands/erickemmanuel.png',
         alt: 'erick emmanuel',
+        url: 'https://www.ericemanuel.com'  
     },
     {
         image: '/brands/essentials.png',
         alt: 'essentials',
+        url: 'https://fearofgod.com/en-do'
     },
     {
         image: '/brands/galleryDept.png',
         alt: 'Gallery Dept',
+        url: 'https://www.chromehearts.com'
     },
     {
         image: '/brands/NB.png',
         alt: 'New Balance',
+        url: 'https://www.newbalance.com'
     },
     {
         image: '/brands/Nike.png',
         alt: 'Nike',
+        url: 'https://www.nike.com/es/'
     },
     {
         image: '/brands/RHUDE.png',
         alt: 'RHUDE',
+        url: 'https://rh-ude.com'
     },
     {
         image: '/brands/YZY.png',
         alt: 'Yezzy',
+        url: 'https://www.adidas.com/us'
     },
 ]
 
 export const Brands = () => {
     return (
-        <div className="flex flex-col items-center gap-3 pt-16 pb-12">
-            <h2 className="font-bold text-2xl text-black">Brands available</h2>
-
-            <p className="w-2/3 text-center text-sm md:text-base text-black">
-                Discover our exclusive collection of clothing from top brands at unbeatable prices.
-            </p>
-
-            <div className="grid grid-cols-3 gap-6 mt-8 items-center md:grid-cols-6">
-                {
-                    brands.map( (brand,index) =>
-                        <div key={index}>
-                            <img 
-                        src={brand.image} 
-                        alt={brand.alt} 
-                        className="w-20 h-20 object-contain"/>
-                        </div>
-                    )
-                }
-
-            </div>
-
+      <div className="container mx-auto px-4 flex flex-col items-center text-center gap-6 pt-16 pb-12">
+        <h2 className="font-bold text-5xl text-black">Brands available</h2>
+  
+        <p className="w-full max-w-2xl text-lg text-black">
+          Discover our exclusive collection of clothing from top brands at unbeatable prices.
+        </p>
+  
+        <div className="grid grid-cols-3 gap-x-12 gap-y-8 mt-8 justify-center ">
+          {brands.map((brand, index) => (
+            <a
+              key={index}
+              href={brand.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex flex-col items-center justify-center h-32 w-32 md:h-40 md:w-40 bg-gradient-to-b from-black/10 via-black/5 to-transparent px-6 py-4 rounded-lg"
+            >
+              <img
+                src={brand.image}
+                alt={brand.alt}
+                className="w-24 h-24 object-contain"
+              />
+            </a>
+          ))}
         </div>
-    )
-}
+      </div>
+    );
+  };
+  
