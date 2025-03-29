8c59da16629994cfbf8963030c6b1ff64b889a11
Angel Aquino <aa23-0272@unphu.edu.do>
2025-03-29 01:40:37 -0400

    add bento grid

--- Ecommerce/src/pages/About.tsx
+++ Ecommerce/src/pages/About.tsx
@@ -1,15 +1,76 @@
 export const About = () => {
     return (
     
-    <div className="grid grid-cols-12 grid-rows-12 gap-5 bg-white">
-        <div className="col-span-3 row-span-4 bg-gray-100 p-6 rounded-lg">7</div>
-        <div className="col-span-6 row-span-4 col-start-4 bg-blue-100 p-6 rounded-lg">8</div>
-        <div className="col-span-3 row-span-4 col-start-10 bg-gray-200 p-6 rounded-lg">9</div>
-        <div className="col-span-6 row-span-4 row-start-5 bg-green-100 p-6 rounded-lg">10</div>
-        <div className="col-span-6 row-span-4 col-start-7 row-start-5 bg-yellow-100 p-6 rounded-lg">12</div>
-        <div className="col-span-3 row-span-4 row-start-9 bg-red-100 p-6 rounded-lg">13</div>
-        <div className="col-span-6 row-span-4 col-start-4 row-start-9 bg-indigo-100 p-6 rounded-lg">14</div>
-        <div className="col-span-3 row-span-4 col-start-10 row-start-9 bg-purple-100 p-6 rounded-lg">15</div>
-    </div>   
+        <div className="grid xl:grid-cols-3 xl:grid-rows-6 md:grid-cols-2 grid-cols-1 gap-5 h-full">
+        <div className="col-span-1 xl:row-span-3">
+          <div className="grid-container">
+            <img src="assets/grid1.png" alt="grid-1" className="w-full sm:h-[276px] h-fit object-contain" />
+
+            <div>
+              <p className="grid-headtext">Hi, I’m Adrian Hajdin</p>
+              <p className="grid-subtext">
+                With 12 years of experience, I have honed my skills in both frontend and backend dev, creating dynamic
+                and responsive websites.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-span-1 xl:row-span-3">
+          <div className="grid-container">
+            <img src="assets/grid2.png" alt="grid-2" className="w-full sm:h-[276px] h-fit object-contain" />
+
+            <div>
+              <p className="grid-headtext">Tech Stack</p>
+              <p className="grid-subtext">
+                I specialize in a variety of languages, frameworks, and tools that allow me to build robust and scalable
+                applications
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-span-1 xl:row-span-4">
+          <div className="grid-container">
+            <div className="rounded-3xl w-full sm:h-[326px] h-fit flex justify-center items-center">
+              
+            </div>
+            <div>
+              <p className="grid-headtext">I’m very flexible with time zone communications & locations</p>
+              <p className="grid-subtext">I&apos;m based in Rjieka, Croatia and open to remote work worldwide.</p>
+              
+            </div>
+          </div>
+        </div>
+
+        <div className="xl:col-span-2 xl:row-span-3">
+          <div className="grid-container">
+            <img src="assets/grid3.png" alt="grid-3" className="w-full sm:h-[266px] h-fit object-contain" />
+
+            <div>
+              <p className="grid-headtext">My Passion for Coding</p>
+              <p className="grid-subtext">
+                I love solving problems and building things through code. Programming isn&apos;t just my
+                profession—it&apos;s my passion. I enjoy exploring new technologies, and enhancing my skills.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="xl:col-span-1 xl:row-span-2">
+          <div className="grid-container">
+            <img
+              src="assets/grid4.png"
+              alt="grid-4"
+              className="w-full md:h-[126px] sm:h-[276px] h-fit object-cover sm:object-top"
+            />
+
+            <div className="space-y-2">
+              <p className="grid-subtext text-center">Contact me</p>
+                <p className="lg:text-2xl md:text-xl font-medium text-gray_gradient text-black">adrian@jsmastery.pro</p>
+              </div>
+            </div>
+          </div>
+        </div>
     )
 }
