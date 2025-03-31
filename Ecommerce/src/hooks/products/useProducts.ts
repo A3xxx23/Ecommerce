93b65aba9bd046891adbea4e28cc6b6b9214569c
Angel Aquino <aa23-0272@unphu.edu.do>
2025-03-31 19:01:39 -0400

    add useProduct

---
+++ Ecommerce/src/hooks/products/useProducts.ts
@@ -0,0 +1,12 @@
+import { useQuery } from "@tanstack/react-query"
+import { getProducts } from "../../actions"
+
+export const useProducts = () => {
+    const {data, isLoading} = useQuery({
+        queryKey: ['products'],
+        queryFn: () => getProducts(),
+        staleTime: 1000 * 60 * 5 //1 hora de productos frescos
+    });
+
+    return {products: data, isLoading};
+};
