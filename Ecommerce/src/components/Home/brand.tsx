82b9ef7713ac713a519f042c98c0627afc0e9467
Angel Aquino <aa23-0272@unphu.edu.do>
2025-04-26 21:36:58 -0400

    add change in metatag and brand grids

--- Ecommerce/src/components/Home/brand.tsx
+++ Ecommerce/src/components/Home/brand.tsx
@@ -47,32 +47,33 @@ const brands = [
 ]
 
 export const Brands = () => {
-    return (
+  return (
       <div className="container mx-auto px-4 flex flex-col items-center text-center gap-6 pt-16 pb-12">
-        <h2 className="font-bold text-5xl text-black">Brands available</h2>
-  
-        <p className="w-full max-w-2xl text-lg text-black">
-          Discover our exclusive collection of clothing from top brands at unbeatable prices.
-        </p>
-  
-        <div className="grid grid-cols-3 gap-x-12 gap-y-8 mt-8 justify-center ">
-          {brands.map((brand, index) => (
-            <a
-              key={index}
-              href={brand.url}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="flex flex-col items-center justify-center h-32 w-32 md:h-40 md:w-40 bg-gradient-to-b from-black/10 via-black/5 to-transparent px-6 py-4 rounded-lg hover:opacity-80"
-            >
-              <img
-                src={brand.image}
-                alt={brand.alt}
-                className="w-24 h-24 object-contain"
-              />
-            </a>
-          ))}
-        </div>
+          <h2 className="font-bold text-5xl text-black">Brands available</h2>
+
+          <p className="w-full max-w-2xl text-lg text-black">
+              Discover our exclusive collection of clothing from top brands at unbeatable prices.
+          </p>
+
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-x-12 gap-y-8 mt-8 justify-center">
+              {brands.map((brand, index) => (
+                  <a
+                      key={index}
+                      href={brand.url}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="flex flex-col items-center justify-center h-32 w-32 md:h-40 md:w-40 bg-gradient-to-b from-black/10 via-black/5 to-transparent px-6 py-4 rounded-lg hover:opacity-80"
+                  >
+                      <img
+                          src={brand.image}
+                          alt={brand.alt}
+                          className="w-24 h-24 object-contain"
+                      />
+                  </a>
+              ))}
+          </div>
       </div>
-    );
-  };
+  );
+};
+
   
